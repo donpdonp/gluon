162c5a7d6150131932baabaa5d3ce866a8bdd96c
Don Park <donp@donp.org>
2014-11-28 15:30:36 -0800

    error handling

--- src/main.c
+++ src/main.c
@@ -24,23 +24,29 @@ main() {
   while(redisGetReply(redis, (void**)&reply) == REDIS_OK) {
     // consume message
     const char* json_in = reply->element[2]->str;
-    puts(json_in);
+    printf("<- %s\n", json_in);
     jvalue = json_parse_string(json_in);
     if(json_value_get_type(jvalue) == JSONObject){
       JSON_Object* obj = json_value_get_object(jvalue);
       const char* code = json_object_get_string(obj, "code");
 
-      const char* json_result;
-      json_result = do_mruby_json(code);
+      if(code){
+        const char* json_result;
+        json_result = do_mruby_json(code);
 
-      if(json_result){
-        puts(json_result);
-      } else {
-        puts("bad code");
+        const char* answer;
+        if(json_result){
+          printf("-> %s\n", json_result);
+          answer = json_result;
+        } else {
+          puts("bad code");
+          answer = "{\"error\":\"bad code\"}";
+        }
+        reply_pub = redisCommand(redis_pub, "publish %s %s", "neur0n", answer);
+        freeReplyObject(reply_pub);
       }
-      reply_pub = redisCommand(redis_pub, "publish %s %s", "neur0no", json_result);
-      freeReplyObject(reply_pub);
     }
+    json_value_free(jvalue);
   }
   freeReplyObject(reply);
 }
@@ -62,14 +68,14 @@ do_mruby_json(const char* code){
   if (0 < parser_state->nerr) {
     printf("line %d: %s\n", parser_state->error_buffer[0].lineno,
                             parser_state->error_buffer[0].message);
-    return;
+    return NULL;
   }
 
   struct RProc* proc;
   proc = mrb_generate_code(state, parser_state);
   if (proc == NULL) {
     fputs("codegen error\n", stderr);
-    return;
+    return NULL;
   }
   mrb_parser_free(parser_state);
 
