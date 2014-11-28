6aff83c1f3a97456220071b04de54b8d0bf90936
Don Park <donp@donp.org>
2014-11-28 14:58:30 -0800

    add redis to ircd

--- services/ircd/index.js
+++ services/ircd/index.js
@@ -1,3 +1,7 @@
+var redisLib = require("redis"),
+    redisSub = redisLib.createClient(),
+    redisPub = redisLib.createClient()
+
 var IrcSocket = require('irc-socket');
 var irc = IrcSocket({
     server: 'irc.freenode.net',
@@ -13,3 +17,8 @@ irc.once('ready', function () {
 
 irc.connect();
 
+redisSub.on("message", function (channel, message) {
+  console.log("redis channel " + channel + ": " + message);
+})
+
+redisSub.subscribe("neur0n")
