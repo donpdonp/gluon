e3113cdd2bada95ab8a15026c2c6acee8c5f9a17
Don Park <donp@donp.org>
2014-11-29 04:45:50 -0800

    multi-vm steps

--- src/main.c
+++ src/main.c
@@ -8,20 +8,34 @@
 
 #define CONFIG(key) json_object_dotget_string(config, key)
 
-const char* do_mruby_json(const char*);
+struct ruby_vm_t {
+  mrb_state* state;
+  const char* owner;
+};
+typedef struct ruby_vm_t ruby_vm;
+ruby_vm* machines;
+ruby_vm admin_vm;
+
+const char* eval_mruby_json(ruby_vm, const char*);
 const char* mruby_stringify_json(mrb_state* mrb, mrb_value val);
 void mainloop(JSON_Object* config);
+void setup();
 
 int
 main() {
-  JSON_Value *config_json;
-  config_json = json_parse_file("config.json");
+  JSON_Value *config_json = json_parse_file("config.json");
   if(json_value_get_type(config_json) == JSONObject){
     JSON_Object* config = json_value_get_object(config_json);
+    setup();
     mainloop(config);
   } else {
     puts("error reading/parsing config.json");
   }
+}
+
+void
+setup() {
+  admin_vm.state = mrb_open();
 }
 
 void
@@ -46,7 +60,7 @@ mainloop(JSON_Object* config) {
 
       if(code){
         const char* json_result;
-        json_result = do_mruby_json(code);
+        json_result = eval_mruby_json(admin_vm, code);
 
         const char* answer;
         if(json_result){
@@ -54,7 +68,6 @@ mainloop(JSON_Object* config) {
           answer = json_result;
         } else {
           puts("bad code");
-          answer = "{\"error\":\"bad code\"}";
         }
         reply_pub = redisCommand(redis_pub, "publish %s %s", "neur0n", answer);
         freeReplyObject(reply_pub);
@@ -66,44 +79,42 @@ mainloop(JSON_Object* config) {
 }
 
 const char*
-do_mruby_json(const char* code){
-  mrb_state* state;
-  state = mrb_open();
-
+eval_mruby_json(ruby_vm vm, const char* code){
   mrbc_context* context;
-  context = mrbc_context_new(state);
+  context = mrbc_context_new(vm.state);
 
   struct mrb_parser_state* parser_state;
-  parser_state = mrb_parse_string(state, code, context);
+  parser_state = mrb_parse_string(vm.state, code, context);
   if (parser_state == NULL) {
     fputs("parse error\n", stderr);
     return;
   }
+
   if (0 < parser_state->nerr) {
-    printf("line %d: %s\n", parser_state->error_buffer[0].lineno,
-                            parser_state->error_buffer[0].message);
-    return NULL;
+    static char err[2048];
+    sprintf(err, "{\"error\":\"line %d: %s\"}", parser_state->error_buffer[0].lineno+1,
+                                                parser_state->error_buffer[0].message);
+    return err;
   }
 
   struct RProc* proc;
-  proc = mrb_generate_code(state, parser_state);
+  proc = mrb_generate_code(vm.state, parser_state);
   if (proc == NULL) {
     fputs("codegen error\n", stderr);
-    return NULL;
+    return "{\"error\":\"codegen\"}";
   }
   mrb_parser_free(parser_state);
 
   mrb_value root_object;
-  root_object = mrb_top_self(state);
+  root_object = mrb_top_self(vm.state);
 
   mrb_value result;
-  result = mrb_run(state, proc, root_object);
+  result = mrb_run(vm.state, proc, root_object);
   if(result.tt == MRB_TT_EXCEPTION){
     fputs("EXCEPTION\n", stderr);
-    return NULL;
+    return "{\"error\":\"ruby exception\"}";
   }
-  const char* json = mruby_stringify_json(state, result);
-  mrb_close(state);
+  const char* json = mruby_stringify_json(vm.state, result);
   return json;
 }
 
@@ -112,4 +123,9 @@ mruby_stringify_json(mrb_state* mrb, mrb_value val) {
   struct RClass* clazz = mrb_module_get(mrb, "JSON");
   mrb_value str = mrb_funcall(mrb, mrb_obj_value(clazz), "stringify", 1, val);
   return mrb_string_value_cstr(mrb, &str);
+}
+
+void
+machines_add(ruby_vm machines, const char* name){
+
 }
