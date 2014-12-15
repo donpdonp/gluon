d0c048db7c42ee67a51d57764e9e8123ac7ed4cb
Don Park <donp@donp.org>
2014-12-15 11:49:15 -0800

    load a new vm with url contents

--- admin.rb
+++ admin.rb
@@ -3,10 +3,13 @@ puts "I am admin.rb"
 module Neur0n
   def self.dispatch(msg)
     puts "I AM DIspatch #{msg.inspect}"
-    if msg["code"]
-      puts "Adding machine mac-a"
-      Neur0n::machine_add("mac-a")
-      Neur0n::machine_eval("mac-a")
+    if msg['name']
+      puts "Adding machine #{msg['name']}"
+      Neur0n::machine_add(msg['name'])
+      if msg["url"]
+        puts "loading #{msg['url']}"
+        code = Neur0n::http_get(msg['url'])
+      end
     end
   end
 end
