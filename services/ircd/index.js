3bb794e42638d0b4b7c005010f28e195810c2d84
Don Park <donp@donp.org>
2014-12-26 13:50:20 -0800

    parse ircd capabilities

--- services/ircd/index.js
+++ services/ircd/index.js
@@ -3,6 +3,9 @@ var redisLib = require("redis"),
     redisPub = redisLib.createClient()
 
 var IrcSocket = require('irc-socket');
+
+var sessions = {}
+
 var irc = IrcSocket({
     server: 'irc.freenode.net',
     port: 6667,
@@ -11,8 +14,15 @@ var irc = IrcSocket({
     });
 
 irc.once('ready', function () {
-  console.log("connected")
-  irc.end();
+  console.log("irc connected")
+})
+
+irc.on('data', function (message) {
+  var ircmsg = /^:[^ ]+ (\d+) [^ ]+ (.*)/.exec(message)
+  if(ircmsg && ircmsg[1] == "005") {
+    var capstr = ircmsg[2].match(/(.*)\s+:[^:]+$/)
+    var capabilities = split005(capstr[1])
+  }
 })
 
 irc.connect();
@@ -22,3 +32,16 @@ redisSub.on("message", function (channel, message) {
 })
 
 redisSub.subscribe("neur0n")
+
+function split005(capstr) {
+  console.log(capstr)
+  var caps = capstr.split(' ')
+  for(var idx in caps) {
+    var kv = caps[idx].split('=')
+    if(kv[1]) {
+      var vs = kv[1].split(',')
+      if(vs.length > 1) { kv[1] = vs}
+    }
+    console.log(kv)
+  }
+}
