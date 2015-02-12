1ad97a46077f492111390c152312804d77cbdcf6
Don Park <donp@donp.org>
2015-02-11 22:00:56 -0800

    vm.add

--- admin.rb
+++ admin.rb
@@ -1,13 +1,15 @@
 module Neur0n
   def self.dispatch(msg)
     puts "admin.rb dispatch #{msg.inspect}"
-    if msg['name']
-      puts "Adding machine #{msg['name']}"
-      idx = Neur0n::machine_add(msg['name'])
-      if idx && msg["url"]
-        puts "loading #{msg['url']}"
-        code = Neur0n::http_get(msg['url'])
-        Neur0n::machine_eval(msg['name'], code)
+    if msg['type'] == 'vm.add'
+      if msg['name']
+        puts "Adding machine #{msg['name']}"
+        idx = Neur0n::machine_add(msg['name'])
+        if idx && msg["url"]
+          puts "loading #{msg['url']}"
+          code = Neur0n::http_get(msg['url'])
+          Neur0n::machine_eval(msg['name'], code)
+        end
       end
     end
   end
