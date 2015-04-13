a5e0f0f4baf5417569bbbe48499f53bf227a64b4
Don Park <donp@donp.org>
2015-04-12 17:48:15 -0700

    manage machine list, and all extra attributes, in admin.rb

--- admin.rb
+++ admin.rb
@@ -1,11 +1,18 @@
-module Neur0n
-  def self.dispatch(msg)
+class MasterControlProgram
+  def initialize
+    @machines = {}
+  end
+
+  def dispatch(msg)
     puts "admin.rb dispatch #{msg.inspect}"
     if msg['type'] == 'vm.add'
       if msg['name']
-        puts "Adding machine #{msg['name']}"
-        idx = Neur0n::machine_add(msg['name'])
+        machine = { id: newId, name: msg['name'] }
+        puts "Adding machine #{machine}"
+        idx = Neur0n::machine_add(machine[:id])
+        @machines[machine['id']] = machine
         if idx && msg["url"]
+          machine[:url] = msg['url']
           puts "loading #{msg['url']}"
           code = Neur0n::http_get(msg['url'])
           Neur0n::machine_eval(msg['name'], code)
@@ -13,7 +20,25 @@ module Neur0n
       end
     end
     if msg['type'] == 'vm.list'
-      {machines: Neur0n::machine_list}
+      #{machines: Neur0n::machine_list}
+      puts "list #{@machines.inspect}"
+      @machines
     end
+  end
+
+  def newId
+    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+    name = ""
+    16.times{ name += alphabet[rand(36)]}
+    name
+  end
+end
+
+MCP = MasterControlProgram.new
+
+module Neur0n
+  def self.dispatch(msg)
+    puts "MCP relay #{msg}"
+    MCP.dispatch(msg)
   end
 end
--- build_config.rb
+++ build_config.rb
@@ -23,6 +23,7 @@ MRuby::Build.new do |conf|
   conf.gembox 'default'
   conf.gem :github => 'mattn/mruby-json'
   conf.gem :github => 'mattn/mruby-pcre-regexp'
+  conf.gem :github => 'iij/mruby-simple-random'
 
   # C compiler settings
   # conf.cc do |cc|
