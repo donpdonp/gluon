3bd8c75fea9aa992e1efdb01bd74adb04d3c9628
Don Park <donp@donp.org>
2015-12-19 12:02:09 -0800

    no method msgs

--- golang/src/donpdonp/gluon/gluon.go
+++ golang/src/donpdonp/gluon/gluon.go
@@ -16,28 +16,31 @@ func main() {
 
   for {
     msg := <-bus.Pipe
-    method := msg["method"].(string)
-    fmt.Println("method: "+method)
+    mo := msg["method"]
+    if mo != nil {
+      method := mo.(string)
+      fmt.Println("method: "+method)
 
-    switch method {
-    case "vm.add":
-      params := msg["params"].(map[string]interface{})
-      url := params["url"].(string)
-      name := params["name"].(string)
-      new_vm := vm.Factory(name)
-      new_vm.Load(url)
-    case "irc.privmsg":
-      for _, vm := range vm.List {
-        fmt.Println("VM "+vm.Name)
+      switch method {
+      case "vm.add":
         params := msg["params"].(map[string]interface{})
-        p1 := params["msg"].(string)
-        call_js := "go(\""+p1+"\")"
-        fmt.Println("js call: "+call_js)
-        value, err := vm.Js.Run(call_js)
-        if err != nil {
-          bus.Send(map[string]interface{}{"error":err.Error()})
-        } else {
-          bus.Send(map[string]interface{}{"result":value.String()})
+        url := params["url"].(string)
+        name := params["name"].(string)
+        new_vm := vm.Factory(name)
+        new_vm.Load(url)
+      case "irc.privmsg":
+        for _, vm := range vm.List {
+          fmt.Println("VM "+vm.Name)
+          params := msg["params"].(map[string]interface{})
+          p1 := params["msg"].(string)
+          call_js := "go(\""+p1+"\")"
+          fmt.Println("js call: "+call_js)
+          value, err := vm.Js.Run(call_js)
+          if err != nil {
+            bus.Send(map[string]interface{}{"error":err.Error()})
+          } else {
+            bus.Send(map[string]interface{}{"result":value.String()})
+          }
         }
       }
     }
