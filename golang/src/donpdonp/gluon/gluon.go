07ca4a3f307728c80f2ac5c598e9a6bedfc541d2
Don Park <donp@donp.org>
2015-12-22 17:26:09 -0800

    clocktower

--- golang/src/donpdonp/gluon/gluon.go
+++ golang/src/donpdonp/gluon/gluon.go
@@ -2,6 +2,7 @@ package main;
 
 import (
   "fmt"
+  "time"
   "encoding/json"
   "net/http"
   "io/ioutil"
@@ -13,13 +14,18 @@ import (
   "github.com/satori/go.uuid"
 )
 
+var (
+  my_uuid uuid.UUID
+)
+
 func main() {
   bus := comm.PubsubFactory()
-  my_uuid := uuid.NewV4()
+  my_uuid = uuid.NewV4()
   bus.Start("localhost:6379")
   go bus.Loop()
 
   fmt.Println("bus started")
+  go clocktower(bus)
 
   for {
     msg := <-bus.Pipe
@@ -74,10 +80,9 @@ func vm_add(name string, url string, bus comm.Pubsub, my_uuid string) {
 
 func dispatch(msg map[string]interface{}, bus comm.Pubsub, my_uuid string) {
   for _, vm := range vm.List {
-    fmt.Println("VM "+vm.Name)
     pprm, _ := json.Marshal(msg)
     call_js := "go("+string(pprm)+")"
-    fmt.Println("js call: "+call_js)
+    fmt.Println("**VM", vm.Name, ": ", call_js)
     value, err := vm.Js.Run(call_js)
     if err != nil {
       bus.Send(irc_reply(msg, err.Error(), my_uuid))
@@ -97,4 +102,14 @@ func irc_reply(msg map[string]interface{}, value string, my_uuid string) (map[st
                                           "channel": params["channel"],
                                           "message": value}
   return resp
+}
+
+func clocktower(bus comm.Pubsub) {
+  for {
+    resp := map[string]interface{}{"id": uuid.NewV4(), "from": my_uuid, "method":"clocktower"}
+    resp["params"] = map[string]interface{}{"now": time.Now().UTC().Format("2006-01-02T15:04:05Z")}
+
+    bus.Send(resp)
+    time.Sleep(60 * time.Second)
+  }
 }
