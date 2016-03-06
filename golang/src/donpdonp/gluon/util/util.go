a2dfa2f7bc10e2b77e590364c8615dfa1ce40fdf
Don Park <donp@donp.org>
2016-03-06 11:53:53 -0800

    util

---
+++ golang/src/donpdonp/gluon/util/util.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+  "encoding/base64"
+  "crypto/sha1"
+
+  "github.com/satori/go.uuid"
+)
+
+
+func Sha1Base64(word string) string {
+  word_sha := sha1.Sum([]byte(word))
+  word_b64 := base64.StdEncoding.EncodeToString(word_sha[:])
+  return word_b64
+}
+
+func Snowflake() string {
+  return uuid.NewV4().String()[0:8]
+}
