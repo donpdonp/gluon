493070334bb077db3880fcc51259b1a42b4b7cf4
Don Park <donp@donp.org>
2018-10-26 15:05:46 -0700

    more mruby

--- golang/src/donpdonp/gluon/vm/mruby.go
+++ golang/src/donpdonp/gluon/vm/mruby.go
@@ -4,13 +4,35 @@ package vm
 #cgo CFLAGS: -I../../../../../mruby/include
 #cgo LDFLAGS: -L../../../../../mruby/build/host/lib -lmruby -lm
 #include "mruby.h"
+
+extern mrb_value Emit(mrb_state*, mrb_value);
+//MRB_API void mrb_define_class_method(mrb_state *, struct RClass *, const char *, mrb_func_t, mrb_aspec);
+static void go_mrb_define_class_method(mrb_state *a, struct RClass *b, const char *c, int d, mrb_aspec e) {
+  mrb_define_class_method(a,b,c,Emit,e);
+}
+
+// cgo doesnt like this define
+//#define MRB_ARGS_REQ(n)     ((mrb_aspec)((n)&0x1f) << 18)
+static mrb_aspec args_req(int n) { return ((n)&0x1f) << 18; }
 */
 import "C"
 
 import "fmt"
 
-func justhere() {
-  a := C.mrb_state{}
-  fmt.Printf("%v\n", a.top_self)
-  C.mrb_open()
+func rubyfactory() {
+  state := C.mrb_open()
+  fmt.Printf("%v\n", state.top_self)
+  ruby_class := C.mrb_define_module(state, C.CString("Gluon"));
+
+  C.go_mrb_define_class_method(
+    state,
+    ruby_class,
+    C.CString("emit"),
+    0,
+    C.args_req(1))
+}
+
+//export Emit
+func Emit(state *C.mrb_state, value C.mrb_value) C.mrb_value {
+  return C.mrb_value{}
 }
