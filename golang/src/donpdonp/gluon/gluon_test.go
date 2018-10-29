c38c6ef6d9aecc3492bc64b4bdf0e092cd656ab3
Don Park <donp@donp.org>
2018-10-29 13:00:36 -0700

    test, not test

---
+++ golang/src/donpdonp/gluon/gluon_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestpickLang(t *testing.T) {
+  url := "https://"
+  contentType := "text/plain"
+  if pickLang(url, contentType) != "javascript" {
+    t.Error("")
+  }
+}
