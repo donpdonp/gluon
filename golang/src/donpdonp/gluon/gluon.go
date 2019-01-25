1c427270301368fbd499e62caa21151b43241942
Don Park <donp@donp.org>
2019-01-25 10:58:33 -0800

    fancier http.get

--- golang/src/donpdonp/gluon/gluon.go
+++ golang/src/donpdonp/gluon/gluon.go
@@ -242,24 +242,39 @@ func vm_enhance_js_standard(vm *vm.VM, bus comm.Pubsub) {
 		"host_id":       util.Settings.Id})
 	vm.Js.Set("http", map[string]interface{}{
 		"get": func(call otto.FunctionCall) otto.Value {
-			var ottoStr otto.Value
+			var ottoV otto.Value
 			arg0 := call.Argument(0)
 			if arg0.IsString() {
 				urlc := arg0.String()
 				resp, body, err := comm.HttpGet(urlc)
 				var resultDisplay string
 				if err != nil {
-					resultDisplay = fmt.Sprintf("err %v\n", urlc, err)
-					ottoStr, _ = otto.ToValue("")
+					resultDisplay = fmt.Sprintf("err %#v\n", err)
+					ottoV, _ = otto.ToValue("")
 				} else {
-					ottoStr, _ = otto.ToValue(string(body)) // scripts not ready for bytes
+					ottoV, _ = otto.ToValue(string(body)) // scripts not ready for bytes
 					resultDisplay = fmt.Sprintf("%s %d bytes\n", resp.Status, len(body))
 				}
 				fmt.Printf("%s/%s http.get %s %s\n", vm.Owner, vm.Name,  urlc, resultDisplay)
 			} else if arg0.IsObject() {
-				fmt.Printf("%s/%s http.get %#v\n", vm.Owner, vm.Name, arg0)
+				urlo := arg0.Object()
+				urlg, _ := urlo.Get("url")
+				urlc := urlg.String()
+				fmt.Printf("%s/%s http.get %#v\n", vm.Owner, vm.Name, urlc)
+				resp, body, err := comm.HttpGet(urlc)
+				fmt.Printf("go %#v %#v\n", resp, err)
+				goResult := map[string]interface{}{}
+				if err != nil {
+					goResult["error"] = map[string]interface{}{
+						                       "message":err.Error()}
+				} else {
+					goResult["status"] = resp.StatusCode
+					goResult["body"] = string(body)
+				}
+				ottoV, err = vm.Js.ToValue(goResult)
+				fmt.Printf("otto %#v %#v\n", ottoV, err)
 			}
-			return ottoStr
+			return ottoV
 		},
 		"post": func(call otto.FunctionCall) otto.Value {
 			urlc := call.Argument(0).String()
