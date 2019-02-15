1e5f34f91daa93585d79b832eceff1cedea85b42
Don Park <donp@donp.org>
2019-02-15 08:52:25 -0800

    moar err dump

--- golang/src/donpdonp/gluon/vm/js.go
+++ golang/src/donpdonp/gluon/vm/js.go
@@ -5,7 +5,9 @@ import "fmt"
 func (vm *VM) EvalJs(js_code string) (string, error) {
 	result, err := vm.Js.Run(js_code)
 	if err != nil {
-		fmt.Println("vm.EvalJs failed", err, vm.Js.Context().Stacktrace, js_code)
+		context := vm.Js.Context()
+		fmt.Printf("vm.EvalJs failed err: %#v stack: %#v file: %#v line: %#v code: %s\n",
+			  err, context.Stacktrace, context.Filename, context.Line, js_code)
 		return "", err
 	} else {
 		otto_json, err := vm.Js.Call("JSON.stringify", nil, result)
